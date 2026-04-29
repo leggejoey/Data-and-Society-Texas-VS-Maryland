92e6fb4b431549bd346ca06c039bbc222f7cf26f
Joey Legge <leggej@spu.edu>
2026-04-29 14:57:06 -0700

    added the 2020 data for texas from redistricting data hub

--- Redistricting/scriptplaceholder.r
+++ Redistricting/scriptplaceholder.r
@@ -6,14 +6,14 @@ library(ggplot2)
 library(ggredist)
 library(sf)
 
-plan2333 <- st_read("Redistricting/PLANC2333/PLANC2333.shp")
+plan2193 <- st_read("Redistricting/tx_cong_2021/PlanC2193.shp")
 plans_texas <- alarm_50state_plans('TX')
 
-plan2333_data <- left_join(plan2333 ,plans_texas, by = c("District" = "district"))
+plan2193_data <- left_join(plan2193 ,plans_texas, by = c("District" = "district"))
 
-plan2333_data <- plan2333_data |> filter(draw == "cd_2020")
+plan2193_data <- plan2193_data |> filter(draw == "cd_2020")
 
-plan2333_data |> 
+plan2193_data |> 
   ggplot() +
   geom_sf(aes(fill = comp_polsby)) +
   labs(fill = "Polsby compactness") +
