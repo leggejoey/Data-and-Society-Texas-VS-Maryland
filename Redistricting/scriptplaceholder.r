f234bbcfc32f6a6ceb2264bed5531b7b45c96c84
Joey Legge <leggej@spu.edu>
2026-05-19 21:24:30 -0700

    I've split texas up into 5 regions to make it easier to compute the sequential monte carlo, I however have been unsuccessful in getting the redist_smc function to work, I'm not sure why it isn't working.

--- Redistricting/scriptplaceholder.r
+++ Redistricting/scriptplaceholder.r
@@ -40,7 +40,6 @@ plans_maryland |>
   geom_sf(aes(fill = comp_polsby)) +
   labs(fill = "Polsby compactness") +
   scale_fill_gradient(
-    #trans = scales::pseudo_log_trans(sigma = 0.01),
     low = "blue", 
     high = "red",
     #breaks = c(0,0.05,0.15,0.35,0.65,1),
@@ -48,7 +47,56 @@ plans_maryland |>
     limits = c(0,1)) +
   theme_map()
 
-#tmap example:
+new_texas_map <- redist_map(plan2193_data, ndists = 38, pop_tol = 0.03)
+
+#original map
+new_texas_map |> 
+  ggplot() +
+  geom_sf(aes(fill = ndv / (nrv + ndv)), linetype = "dashed") +
+  scale_fill_party_c() +
+  theme_map()
+
+#breaking up the map geometry for an easier calculation for my computer
+texas_q1 <- new_texas_map |> filter(District %in% c(13,19,11,25,12,26,31,24))
+texas_q2 <- new_texas_map |> filter(District %in% c(1,3,4,5,6,30,32,33,17))
+texas_q3 <- new_texas_map |> filter(District %in% c(38,29,7,9,18,36,14,22,2,8))
+texas_q4 <- new_texas_map |> filter(District %in% c(10,35,27,37,31,21,20))
+texas_q5 <- new_texas_map |> filter(District %in% c(15,34,28,16,23))
+
+texas_q1 |>
+  ggplot() + 
+  geom_sf(aes(fill = ndv / (nrv + ndv)), linetype = "dashed") +
+  scale_fill_party_c() +
+  theme_map()
+
+texas_q2 |>
+  ggplot() + 
+  geom_sf(aes(fill = ndv / (nrv + ndv)), linetype = "dashed") +
+  scale_fill_party_c() +
+  theme_map()
+
+texas_q3 |>
+  ggplot() + 
+  geom_sf(aes(fill = ndv / (nrv + ndv)), linetype = "dashed") +
+  scale_fill_party_c() +
+  theme_map()
+
+texas_q4 |>
+  ggplot() + 
+  geom_sf(aes(fill = ndv / (nrv + ndv)), linetype = "dashed") +
+  scale_fill_party_c() +
+  theme_map()
+
+texas_q5 |>
+  ggplot() + 
+  geom_sf(aes(fill = ndv / (nrv + ndv)), linetype = "dashed") +
+  scale_fill_party_c() +
+  theme_map()
+
+#new map still broken :\
+new_texas_map_sim <- redist_smc(texas_q1, nsims = 5, ncores = 5)
+
+  #tmap example:
   #library(tmap)
   #map_wa <- alarm_50state_map("WA")
   #tmap_mode("view") #once per r session
